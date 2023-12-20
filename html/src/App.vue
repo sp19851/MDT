df7472ff4d1483dc593562980603c43e2c3d9cb5
sp19851 <sp19851@gmail.com>
2023-12-20 09:16:22 +0200

    init

---
+++ html/src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="app">
+    <div class="main-container">
+      <Header :name="name"/>
+      <div class="bodypage-container">
+        <div class="navigation-container">
+          Навигация
+           
+          <ul>Кнопка 1</ul>
+          <ul>Кнопка 2</ul>
+          <ul>Кнопка 3</ul>
+          <ul>Кнопка 4</ul>
+          <ul>Кнопка 5</ul>
+
+        </div>
+        <div class="maincontextcontainer">я главная часть</div>
+        
+      </div>
+      
+      <div class="footer">я футер</div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+
+export default {
+  name: 'app',
+  components: {
+    
+    Header
+  },
+  data() {
+    return {
+      name:"офицер Томпсон",
+      characters:[
+        {"citizenID":"fdsfsdf", "name":"AlexLutyi", },
+        {"citizenID":"fdsfsdf", "name":"AlexLutyi", },
+        {"citizenID":"fdsfsdf", "name":"AlexLutyi", },
+        {"citizenID":"fdsfsdf", "name":"AlexLutyi", },
+        {"citizenID":"fdsfsdf", "name":"AlexLutyi", },
+      ],
+    };
+  },
+  destroyed() {
+    window.removeEventListener('message', this.listener);
+  },
+  mounted() {
+    this.listener = window.addEventListener(
+      'message',
+      event => {
+        const item = event.data || event.detail;
+        if (this[item.type]) {
+          this[item.type](item);
+        }
+      },
+      false,
+    );
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+/* Want nice animations? Check out https://github.com/asika32764/vue2-animate */
+/* @import 'https://unpkg.com/vue2-animate/dist/vue2-animate.min.css'; */
+.main-container{
+  display: flex;
+  flex-direction: column;
+  min-height: 95vh;
+  .bodypage-container{
+    flex-grow: 1;
+    display: flex;
+    flex-direction: row;
+    
+    .navigation-container{
+      flex-grow: 1;
+      border: 1px black solid;
+      
+    }
+    .maincontextcontainer{
+      flex-grow: 20;
+      border: 1px black solid;
+    }
+
+    .footer{
+      flex-shrink: 0;
+    }
+
+  }
+}
+</style>
---
+++ html/src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="head-container">
+      <div class="img-container">
+        <img src="../assets/Images/Icons/Police.png" width="96" height="88" alt="">
+      </div>
+      <div class="title-container">
+        <div class="text">Мобильный терминал данных ЛСПД</div>
+        <div class="hello">Добро пожаловать, {{name}}</div>
+      </div>
+     
+      
+    </div>
+    
+  </template>
+  
+  <script>
+  export default {
+    name: 'офицер Райдон',
+    props: {
+      name: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  .head-container{
+    background-color: rgb(4,37,79);
+    color: white;
+    display: flex;
+    flex-direction: row;
+    
+  }
+  .title-container {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    justify-content: center;
+    align-items: center;
+    .text {
+      text-transform: uppercase;
+      font-size: 36px;
+      text-align: center;
+
+    }
+  }
+  </style>
